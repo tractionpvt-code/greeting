64c5017c24a226d7660793ee01ea750730ef4ab1
Traction Pvt <sujeeth2k@gmail.com>
2016-11-06 13:21:49 +0100

    Modified Main

--- src/com/tractionpvt/greeting/Main.java
+++ src/com/tractionpvt/greeting/Main.java
@@ -3,13 +3,23 @@ package com.tractionpvt.greeting;
 public class Main {
 
 	public static void main(String[] args) {
-		// Simple Greeting Service
-		Greeting greeting = new GreetingService();
-		System.out.println(greeting.greet("Traction"));
 		
-		// Fortune Greeting Service
-		greeting = new FortuneService();
-		System.out.println(greeting.greet("Jonathan"));
+		Greeting greeting = null;
+		int selection = 0;
+		switch(selection) {
+			case 1:
+				// Simple Greeting Service
+				greeting = new GreetingService();
+				System.out.println(greeting.greet("Traction"));		
+				break;
+			case 2:
+				// Fortune Greeting Service
+				greeting = new FortuneService();
+				System.out.println(greeting.greet("Jonathan"));		
+				break;
+			default:
+				throw new IllegalArgumentException("Must be a valid Selection");
+		}
 		
 	}
 	
