ba02fa2624593f038363c80f12451f1f0bd0fbee
Traction Pvt <sujeeth2k@gmail.com>
2016-11-06 11:54:03 +0100

    Revert "Removed type and added new message to the Messages array"
    This reverts commit 9b82c5ffea453aef70ae64994aa5508059d072a2.

--- src/com/tractionpvt/greeting/FortuneService.java
+++ src/com/tractionpvt/greeting/FortuneService.java
@@ -4,16 +4,15 @@ import java.util.Random;
 
 public class FortuneService implements Greeting {
 
-	private String[] messages = { "Your stars are gonna shine this week :)",
+	private String[] messages = { "Your Stars are gonna shine this week :)",
 			"You need to take care of your health",
-			"Make hay when the sun shines :)",
 			"Its time to move on I guess :)", "Be little careful at work" };
 
 	private Random random = new Random();
-
+	
 	@Override
 	public String greet(String name) {
-		return "Hi " + name + "," + messages[random.nextInt(messages.length)];
+		return "Hi " + name +"," + messages[random.nextInt(messages.length)];
 	}
 
 }
