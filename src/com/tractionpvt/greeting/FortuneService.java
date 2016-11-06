628d938042f5fc34dd998c0d57e491f5c47d4bd4
Traction Pvt <sujeeth2k@gmail.com>
2016-11-06 13:24:30 +0100

    Added new message to Messages array

--- src/com/tractionpvt/greeting/FortuneService.java
+++ src/com/tractionpvt/greeting/FortuneService.java
@@ -5,14 +5,15 @@ import java.util.Random;
 public class FortuneService implements Greeting {
 
 	private String[] messages = { "Your stars are gonna shine this week :)",
-			"You need to take care of your health","May hay when sun shines :)",
-			"Its time to move on I guess :)", "Be little careful at work","Choose your friends wisely" };
+			"You need to take care of your health",
+			"May hay when sun shines :)", "Its time to move on I guess :)",
+			"Be little careful at work", "Choose your friends wisely" };
 
 	private Random random = new Random();
-	
+
 	@Override
 	public String greet(String name) {
-		return "Hi " + name +"," + messages[random.nextInt(messages.length)];
+		return "Hi " + name + "," + messages[random.nextInt(messages.length)];
 	}
 
 }
